d301fdd8b7565ee4417865b1d3f9de08e57f0d56
robert vieira <r0bertsv@outlook.com>
2024-07-02 18:32:44 -0300

    SensorHCSR04

---
+++ codsensorultrassom.ino
@@ -0,0 +1,25 @@
+#include <HCSR04.h>
+
+#define p_trigger 9
+#define p_echo 10
+
+UltraSonicDistanceSensor distanceSensor(p_trigger, p_echo);
+
+int dist_cm; 
+float dist_m;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  dist_cm=distanceSensor.measureDistanceCm();
+  dist_m=dist_cm/100;
+  Serial.print("distancia: ");
+  Serial.print(dist_cm);
+  Serial.print("cm | ");
+  Serial.print(dist_m);
+  Serial.println("m");
+  Serial.println("----------------------");
+  delay(1000);
+}
